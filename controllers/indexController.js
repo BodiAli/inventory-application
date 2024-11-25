c1759a0a3ac93bfa9050dd222bf1603a999f0efb
Bodi Ali <bodiali59@gmail.com>
2024-11-25 05:22:33 +0200

    Import db to show all categories

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,11 @@
-exports.getIndexPage = (req, res) => {
+const db = require("../db/queries");
+
+// TODO: Show categories in index.ejs navbar
+
+exports.getIndexPage = async (req, res) => {
+  const categories = await db.getAllCategories();
+  console.log(categories);
+
   res.render("index", {
     title: "Home",
     category: "Smart Phones",
