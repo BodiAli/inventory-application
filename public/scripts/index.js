0d99ddb365d87cf4d11c3b3f9833d8466f256817
Bodi Ali <bodiali59@gmail.com>
2024-11-25 05:28:53 +0200

    Toggle class on elements to change style

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,16 @@
+const heroElement = document.querySelector(".hero");
 const hamburgerButton = document.querySelector(".hamburger");
 const navElement = document.querySelector(".nav");
 const listElement = document.querySelector(".nav > ul");
+const spanElement = document.querySelector(".button-container > span");
+const logoElement = document.querySelector(".nav .logo");
+
 function toggleNav(e) {
   e.target.classList.toggle("is-active");
   navElement.classList.toggle("is-active");
   listElement.classList.toggle("is-active");
+  spanElement.classList.toggle("is-active");
+  heroElement.classList.toggle("is-active");
+  logoElement.classList.toggle("is-active");
 }
 hamburgerButton.addEventListener("click", toggleNav);
