0ddd043d451388519e31eac130ecbdccb8df8bd1
Bodi Ali <bodiali59@gmail.com>
2024-11-26 23:42:03 +0200

    Toggle is-active class on button container and hr elements

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,11 @@
 const heroElement = document.querySelector(".hero");
+const buttonContainerElement = document.querySelector(".button-container");
 const hamburgerButton = document.querySelector(".hamburger");
 const navElement = document.querySelector(".nav");
 const listElement = document.querySelector(".nav > ul");
 const spanElement = document.querySelector(".button-container > span");
 const logoElement = document.querySelector(".nav .logo");
+const hrElement = document.querySelector(".nav > hr");
 
 function toggleNav(e) {
   e.target.classList.toggle("is-active");
@@ -12,5 +14,7 @@ function toggleNav(e) {
   spanElement.classList.toggle("is-active");
   heroElement.classList.toggle("is-active");
   logoElement.classList.toggle("is-active");
+  hrElement.classList.toggle("is-active");
+  buttonContainerElement.classList.toggle("is-active");
 }
 hamburgerButton.addEventListener("click", toggleNav);
