cacbb7aff04f851f60911d52bc0a776dd3b754ac
Bodi Ali <bodiali59@gmail.com>
2024-11-26 23:41:34 +0200

    Show categories in index.ejs

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,13 @@
 const db = require("../db/queries");
 
-// TODO: Show categories in index.ejs navbar
-
 exports.getIndexPage = async (req, res) => {
   const categories = await db.getAllCategories();
-  console.log(categories);
 
   res.render("index", {
     title: "Home",
     category: "Smart Phones",
     description:
       "lorem ipsum lorem ipsume lorem ipsum lorem ipsume lorem ipsum lorem ipsume lorem ipsum lorem ipsume",
-    categories: [{ category_name: "koko" }],
+    categories,
   });
 };
