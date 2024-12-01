adc701f20a4ceceb4a377c71ddd9435679d3b050
Bodi Ali <bodiali59@gmail.com>
2024-12-01 06:53:08 +0200

    Create a conroller to get a single category with all it's items

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,7 +1,7 @@
 const db = require("../db/queries");
 
 exports.passCurrentRouteToTemplate = (req, res, next) => {
-  res.locals.currentPath = req.baseUrl;
+  res.locals.currentPath = req.originalUrl;
   next();
 };
 
@@ -10,4 +10,12 @@ exports.getAllCategories = async (req, res) => {
   const [smartPhones, laptops, smartWatches] = await db.getFeaturedCategories();
 
   res.render("categories", { title: "Categories", categories, smartPhones, laptops, smartWatches });
+};
+
+exports.getCategory = async (req, res) => {
+  const { id } = req.params;
+
+  const rows = await db.getCategory(id);
+  const [category] = rows;
+  res.render("category", { title: category.category_name, category, rows });
 };
