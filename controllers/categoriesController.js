d7f6f50378cd4420133d7d01a4269fc62936fcfd
Bodi Ali <bodiali59@gmail.com>
2024-12-02 03:42:54 +0200

    Refactor: conditionally render category image based on name in view instead of controller && check if category has items in getCategory

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -7,15 +7,23 @@ exports.passCurrentRouteToTemplate = (req, res, next) => {
 
 exports.getAllCategories = async (req, res) => {
   const categories = await db.getAllCategories();
-  const [smartPhones, laptops, smartWatches] = await db.getFeaturedCategories();
 
-  res.render("categories", { title: "Categories", categories, smartPhones, laptops, smartWatches });
+  res.render("categories", { title: "Categories", categories });
 };
 
 exports.getCategory = async (req, res) => {
-  const { id } = req.params;
+  const id = Number(req.params.id);
 
   const rows = await db.getCategory(id);
+
   const [category] = rows;
-  res.render("category", { title: category.category_name, category, rows });
+
+  const hasItems = rows.some((row) => row.item_id !== null);
+
+  const isCategoryFeatured =
+    category.category_name === "Smart Phones" ||
+    category.category_name === "Laptops" ||
+    category.category_name === "Smart Watches";
+
+  res.render("category", { title: category.category_name, category, rows, isCategoryFeatured, hasItems });
 };
