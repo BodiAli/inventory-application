af9750cd1ed3b21dc42f21b0955fa9fc229a2e65
Bodi Ali <bodiali59@gmail.com>
2024-12-04 15:24:29 +0200

    Move currentPath controller to app.js && require itemsRouter

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
 const categoriesRouter = require("./routes/categoriesRouter");
+const itemsRouter = require("./routes/itemsRouter");
 
 const app = express();
+
+const passCurrentRouteToTemplate = (req, res, next) => {
+  res.locals.currentPath = req.originalUrl;
+  next();
+};
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
-app.use("/categories", categoriesRouter);
+app.use("/categories", passCurrentRouteToTemplate, categoriesRouter);
+app.use("/items", passCurrentRouteToTemplate, itemsRouter);
 
 const port = process.env.PORT || 3000;
 
