637f397be8bdcff86dd22d07c756ce66462f9203
Bodi Ali <bodiali59@gmail.com>
2024-12-07 09:06:03 +0200

    Handle post and get requests for /create route

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -4,6 +4,8 @@ const itemsController = require("../controllers/itemsController");
 const itemsRouter = Router();
 
 itemsRouter.get("/", itemsController.getAllItems);
+itemsRouter.get("/create", itemsController.getCreateItemForm);
+itemsRouter.post("/create", itemsController.createItem);
 itemsRouter.get("/:id", itemsController.getItem);
 
 module.exports = itemsRouter;
