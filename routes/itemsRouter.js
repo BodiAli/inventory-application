2854b459a52c0e3df61d7e3a84886e01adf9ae31
Bodi Ali <bodiali59@gmail.com>
2024-12-07 15:20:01 +0200

    Handle requests for get and post at route :id/updated

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -6,6 +6,8 @@ const itemsRouter = Router();
 itemsRouter.get("/", itemsController.getAllItems);
 itemsRouter.get("/create", itemsController.getCreateItemForm);
 itemsRouter.post("/create", itemsController.createItem);
+itemsRouter.get("/:id/update", itemsController.getUpdateItemForm);
+itemsRouter.post("/:id/update", itemsController.updateItem);
 itemsRouter.get("/:id", itemsController.getItem);
 
 module.exports = itemsRouter;
