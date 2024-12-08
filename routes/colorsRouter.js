769669e0f831f89eb8ecdab5f53e5b0f6409dd3a
Bodi Ali <bodiali59@gmail.com>
2024-12-08 06:25:36 +0200

    Handle get requests for /colors path

--- routes/colorsRouter.js
+++ routes/colorsRouter.js
@@ -3,6 +3,7 @@ const colorsController = require("../controllers/colorsController");
 
 const colorsRouter = Router();
 
+colorsRouter.get("/", colorsController.getAllColors);
 colorsRouter.post("/create", colorsController.createColor);
 
 module.exports = colorsRouter;
