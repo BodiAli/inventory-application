3749c5350bf1e0d62e7b091bad97b30eb0b4a6ed
Bodi Ali <bodiali59@gmail.com>
2024-12-09 11:41:47 +0200

    Handle delete route

--- routes/colorsRouter.js
+++ routes/colorsRouter.js
@@ -5,6 +5,7 @@ const colorsRouter = Router();
 
 colorsRouter.get("/", colorsController.getAllColors);
 colorsRouter.post("/create", colorsController.createColor);
+colorsRouter.post("/:id/delete", colorsController.deleteColor);
 colorsRouter.get("/:id", colorsController.getColor);
 
 module.exports = colorsRouter;
