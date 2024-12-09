2593ae1e57f45e64908d4ccb2d4d0c236c4a289d
Bodi Ali <bodiali59@gmail.com>
2024-12-09 10:12:36 +0200

    Throw error if no items are found

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -1,7 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 exports.getAboutPage = asyncHandler(async (req, res) => {
   const items = await db.getAllItemsLimit5();
+  if (!items || items.length === 0) {
+    throw new CustomNotFoundError("No items found");
+  }
   res.render("about", { title: "About", items });
 });
