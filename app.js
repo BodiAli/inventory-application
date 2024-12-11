57f1e882ce3568e67609809be157983cdd2b51d3
Bodi Ali <bodiali59@gmail.com>
2024-12-11 16:27:25 +0200

    Fix: Pass port variable instead of hardcoding the port number

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.use(passCurrentRouteToTemplate, (err, req, res, _next) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
 });
